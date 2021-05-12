9cedfd6bcdecd0eb95ee1da1476719c3453a67e5
sowunmi mayowa <smayowa176@gmail.com>
2021-05-12 11:23:19 -0700

    created mobile navigation :tada: :tada:

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector("#menu");
+const mobileNav = document.querySelector(".mobile-nav")
+
+menu.addEventListener('click', toggleNav)
+
+function toggleNav(){
+    menu.src = "./images/icon-close.svg"
+
+    if(mobileNav.style.right == "400px"){
+        mobileNav.style.right = "30px"
+    }
+    else{
+        mobileNav.style.right = "400px"
+        menu.src = "./images/icon-hamburger.svg"
+    }
+    
+}
